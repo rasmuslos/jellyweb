f43e67ce57291a955a13411fa56a3d90deb2408e
Rasmus Krämer <git@rfk.io>
2021-09-15 15:44:59 +0200

    Added global.css

---
+++ src/components/navigation/Navigation.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type {Item} from "$lib/typings/navigation";
+
+    const items: Item[] = [
+        { title: "Movies", href: "/movies", regex: "(\\/?movies\\/?.*)|(\\/)" },
+        { title: "Series", href: "/series", regex: "(\\/?series\\/?.*)|(\\/)" },
+        { title: "Generes", href: "/generes", regex: "(\\/?generes\\/?.*)|(\\/)" },
+    ]
+</script>
+
+<div>
+    <nav></nav>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Navigation from "../components/navigation/Navigation.svelte";
+</script>
+
+<style>
+    div {
+        height: 100%;
+        width: 100%;
+
+        color: var(--text);
+        font-family: var(--font);
+
+        background-color: var(--background);
+        overflow: hidden;
+    }
+</style>
+
+<div>
+    <Navigation />
+</div>
