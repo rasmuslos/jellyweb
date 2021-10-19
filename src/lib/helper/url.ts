c2dd484b2da0cf5957735e1b93bf6e35037d0695
Rasmus Krämer <git@rfk.io>
2021-10-19 17:03:36 +0200

    random background image

--- src/lib/helper/url.ts
+++ src/lib/helper/url.ts
@@ -5,11 +5,19 @@ import {browser} from "$app/env";
 
 export const generatePlayerUrl = (itemId: string, startAt: number = 0) => `/player/${itemId}?start=${startAt}&url=${browser ? window.location.href : "/"}`
 export const generateItemUrl = (itemId: string) => `/detail/${itemId}`
-export const generateImageUrl = (id: string, tag: string, type: "Backdrop" | "Primary", maxWidth: number = null, scope: string = "Items", maxHeight: number = null, jellyfinSession: JellyfinSession = null) =>
-    `${jellyfinSession ? jellyfinSession.server : get(session).active.server}/${scope}/${id}/Images/${type}/?tag=${tag}&quality=90${maxWidth !== null ? `&fillWidth=${maxWidth}` : ""}${maxHeight !== null ? `&fillHeight=${maxHeight}` : ""}`
-export const getLargeBackdrop = ({ BackdropImageTags, Id, SeriesId, SeriesPrimaryImageTag }: Item) =>
-    BackdropImageTags && BackdropImageTags.length > 0
-        ? generateImageUrl(Id, BackdropImageTags[0], `Backdrop`)
-        : SeriesId && SeriesPrimaryImageTag && SeriesPrimaryImageTag.length > 0
-            ? generateImageUrl(SeriesId, SeriesPrimaryImageTag[0], `Backdrop`)
-            : null
+export const generateImageUrl = (id: string, tag: string, type: "Backdrop" | "Primary", maxWidth: number = null, scope: string = "Items", maxHeight: number = null, jellyfinSession: JellyfinSession = null) => generateImageUrlIndex(id, tag, 0, type, maxWidth, scope, maxHeight, jellyfinSession)
+export const generateImageUrlIndex = (id: string, tag: string, index: number, type: "Backdrop" | "Primary", maxWidth: number = null, scope: string = "Items", maxHeight: number = null, jellyfinSession: JellyfinSession = null) =>
+    `${jellyfinSession ? jellyfinSession.server : get(session).active.server}/${scope}/${id}/Images/${type}/${index}?tag=${tag}&quality=90${maxWidth !== null ? `&fillWidth=${maxWidth}` : ""}${maxHeight !== null ? `&fillHeight=${maxHeight}` : ""}`
+const getRandomBackdrop = (id: string, tags: string[]) => {
+    const index = Math.floor(Math.random() * (tags.length - 1))
+
+    console.log(index, tags[index], generateImageUrlIndex(id, tags[index], index, "Backdrop"))
+
+    return generateImageUrlIndex(id, tags[index], index, "Backdrop")
+}
+export const getLargeBackdrop = ({ BackdropImageTags, Id, SeriesId, ParentBackdropImageTags }: Item) => {
+    if(BackdropImageTags && BackdropImageTags.length > 0) return getRandomBackdrop(Id, BackdropImageTags)
+    else if(SeriesId && ParentBackdropImageTags && ParentBackdropImageTags.length > 0) return getRandomBackdrop(SeriesId, ParentBackdropImageTags)
+
+    return null
+}
